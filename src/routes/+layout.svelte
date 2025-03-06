594b3964cd817f91772a9a54a8be8b491f0907a7
TheMoonKnight8 <48894025+sajadb-dev@users.noreply.github.com>
2025-03-06 03:54:05 +0330

    Added Custom Titlebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,13 @@
 <script lang="ts">
-    import '../app.css';
+import '../app.css';
+  import Titlebar from '../components/titlebar.svelte';
+
+let { children } = $props();
 </script>
+
+<div class="w-full h-full flex flex-col justify-between">
+    <div class="layer-1">
+        <Titlebar/>
+    </div>
+</div>
+{@render children()}
